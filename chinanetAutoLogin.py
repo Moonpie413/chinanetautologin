5d671e7d39f4dd2fffbd5b7cf5db32f346a462c8
moonpie <1275582499@qq.com>
2015-10-25 21:13:10 +0800

    用IDE格式化了不规范代码

--- chinanetAutoLogin.py
+++ chinanetAutoLogin.py
@@ -6,18 +6,17 @@ import cookielib
 import json
 import ssl
 
-class connect(object):
-    
+
+class Connect(object):
     def __init__(self):
-        ## 关闭证书验证
+        # 关闭证书验证
         ssl._create_default_https_context = ssl._create_unverified_context
 
         self.request_url = 'https://wlan.ct10000.com/login.wlan'
         self.logout_url = 'https://wlan.ct10000.com/logout.wlan'
         self.otherUser = 'hswl00002832'
         self.otherUserPwd = '677454'
-        self.regArea = 'ah';
-
+        self.regArea = 'ah'
 
         # 初始化cookie
         self.cookie = cookielib.CookieJar()
@@ -27,70 +26,70 @@ class connect(object):
 
     def login(self):
 
-        #头信息
+        # 头信息
         headers = {
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:41.0) Gecko/20100101 Firefox/41.0',
-            'Host' : 'wlan.ct10000.com',
-            'Connection' : 'keep-alive',
-            'Referer' : 'https://wlan.ct10000.com/index.wlan'
+            'Host': 'wlan.ct10000.com',
+            'Connection': 'keep-alive',
+            'Referer': 'https://wlan.ct10000.com/index.wlan'
         }
 
-        #数据
+        # 数据
         data = {
-            'userName' : self.otherUser ,
-            'password' : self.otherUserPwd ,
-            'regArea' : self.regArea ,
-            'userIp' : '' ,
-            'basIp' : '' ,
-            'acName' : '' ,
-            'oraUrl' : '' ,
-            'loginMode' : '2'
+            'userName': self.otherUser,
+            'password': self.otherUserPwd,
+            'regArea': self.regArea,
+            'userIp': '',
+            'basIp': '',
+            'acName': '',
+            'oraUrl': '',
+            'loginMode': '2'
         }
 
-        #迭代添加头信息
-        for key,value in headers.iteritems():
-            #用元组的形式添加headers
-            temp = (key,value)
+        # 迭代添加头信息
+        for key, value in headers.iteritems():
+            # 用元组的形式添加headers
+            temp = (key, value)
             self.opener.addheaders.append(temp)
 
         post_data = urllib.urlencode(data)
 
-        #建立连接，返回response
-        response = self.opener.open(self.request_url,post_data)
+        # 建立连接，返回response
+        response = self.opener.open(self.request_url, post_data)
         return response.read()
 
     def logout(self):
-        encStr = ''
         with open('./Enc.txt') as f:
-            encStr = f.readline()
+            enc_str = f.readline()
         data = {
-            'Enc' : encStr
+            'Enc': enc_str
         }
 
         post_data = urllib.urlencode(data)
 
-        #建立连接，返回response
-        response = self.opener.open(self.logout_url,post_data)
+        # 建立连接，返回response
+        response = self.opener.open(self.logout_url, post_data)
         return response.read()
 
-    #处理response
-    def response_handle(self,response):
-        #将response字符串转为字典
-        response_dict = json.loads(response)
-        susState = response_dict['sucState']
-        respCode = response_dict['respCode']
-        encStr = response_dict['encStr']
-        if susState == 'SUCCESS' and respCode == '0':
-            if encStr:
-                print '连接成功'
-                with open('./Enc.txt','w') as f:
-                    f.write(encStr)
-            else:
-                print '断开成功'
-        if susState == 'FAIL' and respCode == '202':
-            print '连接失败'
+
+# 处理response
+def response_handle(response):
+    # 将response字符串转为字典
+    response_dict = json.loads(response)
+    susstate = response_dict['sucState']
+    respcode = response_dict['respcode']
+    encstr = response_dict['encstr']
+    if susstate == 'SUCCESS' and respcode == '0':
+        if encstr:
+            print '连接成功'
+            with open('./Enc.txt', 'w') as f:
+                f.write(encstr)
+        else:
+            print '断开成功'
+    if susstate == 'FAIL' and respcode == '202':
+        print '连接失败'
 
 # 简单测试
-connect = connect()
+connect = Connect()
 f = connect.login()
-connect.response_handle(f)
+response_handle(f)

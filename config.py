4167e6ff29b598f66679934fc4685775694d0c15
moonpie <1275582499@qq.com>
2015-11-04 20:23:02 +0800

    修复了windows下文件初始化失败的bug

--- config.py
+++ config.py
@@ -5,40 +5,40 @@ import os
 __author__ = 'maroon'
 
 config = ConfigParser.ConfigParser()
-configfile = 'cfgfolder/info.cfg'
+configfile = 'cfgfolder' + os.sep + 'info.cfg'
 
 
 def read_config():
-    with open('cfgfolder/info.cfg', 'r') as f:
+    with open(configfile, 'r') as f:
         config.readfp(f)
-        otherUser = config.get('otherAccount', 'otherUser')
-        otherUserPwd = config.get('otherAccount', 'otherUserPwd')
-        regArea = config.get('otherAccount', 'regArea')
-        result = (otherUser,otherUserPwd,regArea)
+        other_user = config.get('otherAccount', 'otherUser')
+        other_user_pwd = config.get('otherAccount', 'otherUserPwd')
+        reg_area = config.get('otherAccount', 'regArea')
+        result = (other_user, other_user_pwd, reg_area)
     return result
 
 
 def config_check():
     if not os.path.exists(configfile):
         print '配置文件不存在，正在初始化'
-        os.mknod(configfile)
-        init_config()
+        with open(configfile, 'w') as f:
+            init_config(f)
         print '初始化成功，请编辑cfgfolder/info.cfg填入账号信息'
         return False
     else:
-        for v in read_config():
-            if v == '':
-                print '请完整填写配置文件'
-                return False
+        try:
+            for v in read_config():
+                if v == '':
+                    print '请完整填写配置文件'
+                    return False
+        except Exception as e:
+            print e
     return True
 
 
-def init_config():
-    with open(configfile, 'rw+') as f:
-        config.readfp(f)
-        config.add_section('otherAccount')
-        config.set('otherAccount', 'otherUser', '')
-        config.set('otherAccount', 'otherUserPwd', '')
-        config.set('otherAccount', 'regArea', '')
-        config.write(f)
-
+def init_config(f):
+    config.add_section('otherAccount')
+    config.set('otherAccount', 'otherUser', '')
+    config.set('otherAccount', 'otherUserPwd', '')
+    config.set('otherAccount', 'regArea', '')
+    config.write(f)
